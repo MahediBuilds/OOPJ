40942d23ec8a6bb7e25632096e99e7c7fa794580
Zain Mahedi <zainmahedi.cs23@bmsce.ac.in>
2024-10-22 23:01:52 +0530

    Lab 2

--- Fibonacci.java
+++ Fibonacci.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 class Fibonacci {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of terms: ");
         int num = sc.nextInt();
