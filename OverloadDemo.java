5bbab0dc858989b53c59d54098e3e2499324eef8
Zain Mahedi <zainmahedi.cs23@bmsce.ac.in>
2024-10-29 10:31:49 +0530

    Lab 5

--- OverloadDemo.java
+++ OverloadDemo.java
@@ -38,7 +38,6 @@ class Operations {
         Operations opDouble = new Operations(5.0, 7.0);
         Operations opString = new Operations("Hello", " World");
 
-        // Using method overloading to perform different types of additions
         System.out.println("Addition of Integers 5 and 7: " + opInt.add(opInt.a, opInt.b));
         System.out.println("Addition of Doubles 5.0 and 7.0: " + opDouble.add(opDouble.c, opDouble.d));
         System.out.println("Addition of Strings \"Hello\" and \" World\": " + opString.add(opString.str1, opString.str2));
